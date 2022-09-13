e05c2fa24d5e1bfdd1d36d5aa1000dbaecf785a5
V0rt3xqa <111151771+V0rt3xqa@users.noreply.github.com>
2022-09-12 19:08:04 -0500

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,5 +1,5 @@
 pcall(function() 
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/LegoRblx/AlertVapeV4/main/MainLoader.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/V0rt3xqa/AlertVapeV4/main/MainLoader.lua"))()
 end)
 local a=syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()end
 local callback
@@ -7,5 +7,5 @@ callback = game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(b)
       if shared.AlertLoaded == true then
         callback:Disconnect()
       end
-      a("loadstring(game:HttpGet('https://raw.githubusercontent.com/LegoRblx/AlertVapeV4/main/Loadstring.lua'))()")
+      a("loadstring(game:HttpGet('https://raw.githubusercontent.com/V0rt3xqa/AlertVapeV4/blob/main/Loadstring.lua'))()")
 end end)
